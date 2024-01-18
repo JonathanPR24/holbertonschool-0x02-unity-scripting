427f6cd2ec15b18208d93dc1a6d9a13af45a7c9e
Jonathan Perez <jonathanperez604@gmail.com>
2024-01-18 15:31:53 -0400

    update

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,7 @@
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5f; // Adjust this value in the Inspector to change player speed
+    private int score = 0; // New score variable
 
     void Start()
     {
@@ -30,5 +31,20 @@ public class PlayerController : MonoBehaviour
         // Move the player on the X and Z axes only
         Vector3 newPosition = transform.position + movement * speed * Time.fixedDeltaTime;
         transform.position = new Vector3(newPosition.x, transform.position.y, newPosition.z);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Pickup"))
+        {
+            // Increment score when the Player touches an object tagged Pickup
+            score++;
+
+            // Write the new value of score to the console
+            Debug.Log("Score: " + score);
+
+            // Disable or destroy the Coin object
+            Destroy(other.gameObject);
+        }
     }
 }
