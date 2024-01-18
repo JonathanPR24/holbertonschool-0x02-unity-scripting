8d1d03a339e181182d7d9046ede65c0d6a87d178
Jonathan Perez <jonathanperez604@gmail.com>
2024-01-18 13:48:27 -0400

    Bust a move

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f; // Adjust this value in the Inspector to change player speed
+
+    void Start()
+    {
+        // Initialization code if needed
+    }
+
+    void FixedUpdate()
+    {
+        // Get input from the player
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        // Calculate the movement direction
+        Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput);
+
+        // Move the player
+        MovePlayer(movement);
+    }
+
+    void MovePlayer(Vector3 movement)
+    {
+        // Normalize the movement vector to ensure consistent speed in all directions
+        movement.Normalize();
+
+        // Move the player on the X and Z axes only
+        Vector3 newPosition = transform.position + movement * speed * Time.fixedDeltaTime;
+        transform.position = new Vector3(newPosition.x, transform.position.y, newPosition.z);
+    }
+}
